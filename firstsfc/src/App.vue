b1cd915e1c83bb5e26e4d6cead4c581ebb9a9fa1
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-05-25 03:13:10 +0000

    added supabase

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -5,9 +5,27 @@
   <food-item/>
   <food-item/>
   <food-item/>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
 </template>
 
-<script></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+   getInstruments()
+})
+</script>
+
 <style>
   #app > div {
     border: dashed black 1px;

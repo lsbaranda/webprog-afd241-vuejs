5868eb68deb57ce695c512710cdf3800dbe6db16
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-05-26 02:36:41 +0000

    instruments fix

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -3,6 +3,7 @@
   <h1>Food</h1>
   <food-item/>
   <food-item2/>
+  <h2>Instruments</h2>
   <ul>
     <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
   </ul>
@@ -13,16 +14,25 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+import CommentForm from './components/CommentForm.vue'
 
 const instruments = ref([])
+const comments = ref([])
 
 async function getInstruments() {
   const { data } = await supabase.from('instruments').select()
   instruments.value = data
 }
+async function getComments() {
+  const { data } = await supabase.from('comments').select()
+  comments.value = data
+}
 
 onMounted(() => {
    getInstruments()
+   getComments()
 })
 </script>
 

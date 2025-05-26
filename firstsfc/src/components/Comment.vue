6956e6b04b3315216c82ac13c42af0a572e7767f
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-05-26 03:32:16 +0000

    comments style

--- firstsfc/src/components/Comment.vue
+++ firstsfc/src/components/Comment.vue
@@ -7,15 +7,6 @@
   
   <script></script>
   
-  <style>
-    #app > div {
-      border: dashed black 1px;
-      display: inline-block;
-      margin: 10px;
-      padding: 10px;
-      background-color: lightyellow;
-    }
-  </style>
 
 <script setup>
 import { ref, onMounted } from 'vue'
@@ -36,11 +27,5 @@ onMounted(() => {
 
 
 <style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightyellow;
-  }
+
 </style>
